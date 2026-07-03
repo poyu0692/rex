f509cd170c5f4553d5b5697e0a36fd2339bee2c9
poyu0692 <poyu0692@gmail.com>
2026-07-03 21:22:52 +0900

    refactor: split AST arenas by node kind

--- src/ast.rs
+++ src/ast.rs
@@ -1,83 +1,150 @@
 use crate::diagnostics::Span;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub struct Ast {
-    nodes: Vec<Node>,
-    root: NodeId,
+    modules: Vec<Module>,
+    items: Vec<Item>,
+    stmts: Vec<Stmt>,
+    exprs: Vec<Expr>,
+    root: Option<ModuleId>,
 }
 
 impl Ast {
-    pub fn new(root: Node) -> Self {
-        Self {
-            nodes: vec![root],
-            root: NodeId(0),
-        }
+    pub fn new() -> Self {
+        Self::default()
     }
 
-    pub fn push(&mut self, node: Node) -> NodeId {
-        let id = NodeId(self.nodes.len());
-        self.nodes.push(node);
+    pub fn with_root(root: Module) -> Self {
+        let mut ast = Self::new();
+        let root = ast.push_module(root);
+        ast.set_root(root);
+        ast
+    }
+
+    pub fn push_module(&mut self, module: Module) -> ModuleId {
+        let id = ModuleId(self.modules.len());
+        self.modules.push(module);
         id
     }
 
-    pub fn nodes(&self) -> &[Node] {
-        &self.nodes
+    pub fn push_item(&mut self, item: Item) -> ItemId {
+        let id = ItemId(self.items.len());
+        self.items.push(item);
+        id
     }
 
-    pub fn root_id(&self) -> NodeId {
+    pub fn push_stmt(&mut self, stmt: Stmt) -> StmtId {
+        let id = StmtId(self.stmts.len());
+        self.stmts.push(stmt);
+        id
+    }
+
+    pub fn push_expr(&mut self, expr: Expr) -> ExprId {
+        let id = ExprId(self.exprs.len());
+        self.exprs.push(expr);
+        id
+    }
+
+    pub fn modules(&self) -> &[Module] {
+        &self.modules
+    }
+
+    pub fn items(&self) -> &[Item] {
+        &self.items
+    }
+
+    pub fn stmts(&self) -> &[Stmt] {
+        &self.stmts
+    }
+
+    pub fn exprs(&self) -> &[Expr] {
+        &self.exprs
+    }
+
+    pub fn root_id(&self) -> Option<ModuleId> {
         self.root
     }
 
-    pub fn set_root(&mut self, root: NodeId) -> bool {
-        if self.get_node(root).is_none() {
+    pub fn set_root(&mut self, root: ModuleId) -> bool {
+        if self.get_module(root).is_none() {
             return false;
         }
 
-        self.root = root;
+        self.root = Some(root);
         true
     }
 
-    pub fn node(&self, id: NodeId) -> &Node {
-        self.get_node(id).expect("invalid AST node id")
+    pub fn module(&self, id: ModuleId) -> &Module {
+        self.get_module(id).expect("invalid AST module id")
     }
 
-    pub fn get_node(&self, id: NodeId) -> Option<&Node> {
-        self.nodes.get(id.index())
+    pub fn get_module(&self, id: ModuleId) -> Option<&Module> {
+        self.modules.get(id.index())
     }
 
-    pub fn root(&self) -> &Node {
-        self.node(self.root)
+    pub fn item(&self, id: ItemId) -> &Item {
+        self.get_item(id).expect("invalid AST item id")
+    }
+
+    pub fn get_item(&self, id: ItemId) -> Option<&Item> {
+        self.items.get(id.index())
+    }
+
+    pub fn stmt(&self, id: StmtId) -> &Stmt {
+        self.get_stmt(id).expect("invalid AST statement id")
+    }
+
+    pub fn get_stmt(&self, id: StmtId) -> Option<&Stmt> {
+        self.stmts.get(id.index())
+    }
+
+    pub fn expr(&self, id: ExprId) -> &Expr {
+        self.get_expr(id).expect("invalid AST expression id")
+    }
+
+    pub fn get_expr(&self, id: ExprId) -> Option<&Expr> {
+        self.exprs.get(id.index())
+    }
+
+    pub fn root(&self) -> Option<&Module> {
+        self.root.map(|root| self.module(root))
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct NodeId(usize);
+pub struct ModuleId(usize);
 
-impl NodeId {
+impl ModuleId {
     pub const fn index(self) -> usize {
         self.0
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Node {
-    pub kind: NodeKind,
-    pub span: Span,
-}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ItemId(usize);
 
-impl Node {
-    pub const fn new(kind: NodeKind, span: Span) -> Self {
-        Self { kind, span }
+impl ItemId {
+    pub const fn index(self) -> usize {
+        self.0
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum NodeKind {
-    Module(Module),
-    Item(Item),
-    Expr(Expr),
-    Stmt(Stmt),
-    Error(Error),
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct StmtId(usize);
+
+impl StmtId {
+    pub const fn index(self) -> usize {
+        self.0
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ExprId(usize);
+
+impl ExprId {
+    pub const fn index(self) -> usize {
+        self.0
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -97,29 +164,32 @@ impl Ident {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Module {
-    pub items: Vec<NodeId>,
+    pub items: Vec<ItemId>,
+    pub span: Span,
 }
 
 impl Module {
-    pub fn new(items: Vec<NodeId>) -> Self {
-        Self { items }
+    pub fn new(items: Vec<ItemId>, span: Span) -> Self {
+        Self { items, span }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Item {
     pub kind: ItemKind,
+    pub span: Span,
 }
 
 impl Item {
-    pub const fn new(kind: ItemKind) -> Self {
-        Self { kind }
+    pub const fn new(kind: ItemKind, span: Span) -> Self {
+        Self { kind, span }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ItemKind {
     Function(Function),
+    Error(Error),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -127,7 +197,7 @@ pub struct Function {
     pub name: Ident,
     pub params: Vec<Param>,
     pub return_type: Option<TypeRef>,
-    pub body: Vec<NodeId>,
+    pub body: Vec<StmtId>,
 }
 
 impl Function {
@@ -135,7 +205,7 @@ impl Function {
         name: Ident,
         params: Vec<Param>,
         return_type: Option<TypeRef>,
-        body: Vec<NodeId>,
+        body: Vec<StmtId>,
     ) -> Self {
         Self {
             name,
@@ -196,30 +266,48 @@ pub enum TypeRefKind {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Expr {
-    pub kind: ExprKind,
-}
-
-impl Expr {
-    pub const fn new(kind: ExprKind) -> Self {
-        Self { kind }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Stmt {
     pub kind: StmtKind,
+    pub span: Span,
 }
 
 impl Stmt {
-    pub const fn new(kind: StmtKind) -> Self {
-        Self { kind }
+    pub const fn new(kind: StmtKind, span: Span) -> Self {
+        Self { kind, span }
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum StmtKind {
-    Expr(NodeId),
+    Expr(ExprId),
+    Error(Error),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Expr {
+    pub kind: ExprKind,
+    pub span: Span,
+}
+
+impl Expr {
+    pub const fn new(kind: ExprKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ExprKind {
+    Number(NumberLiteral),
+    Unary {
+        op: UnaryOp,
+        expr: ExprId,
+    },
+    Binary {
+        lhs: ExprId,
+        op: BinaryOp,
+        rhs: ExprId,
+    },
+    Error(Error),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -229,20 +317,6 @@ impl Error {
     pub const fn new() -> Self {
         Self
     }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum ExprKind {
-    Number(NumberLiteral),
-    Unary {
-        op: UnaryOp,
-        expr: NodeId,
-    },
-    Binary {
-        lhs: NodeId,
-        op: BinaryOp,
-        rhs: NodeId,
-    },
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -269,79 +343,71 @@ pub enum BinaryOp {
 mod tests {
     use super::*;
 
-    fn integer(value: &str, span: Span) -> Node {
-        Node::new(
-            NodeKind::Expr(Expr::new(ExprKind::Number(NumberLiteral::Integer(
-                value.into(),
-            )))),
-            span,
-        )
+    fn integer(value: &str, span: Span) -> Expr {
+        Expr::new(ExprKind::Number(NumberLiteral::Integer(value.into())), span)
     }
 
     #[test]
-    fn ast_owns_root_node() {
-        let span = Span::new(0, 3);
-        let ast = Ast::new(integer("123", span));
+    fn ast_starts_without_a_root() {
+        let ast = Ast::new();
 
-        assert_eq!(ast.root_id(), NodeId(0));
-        assert_eq!(ast.root().span, span);
+        assert_eq!(ast.root_id(), None);
+        assert!(ast.root().is_none());
+        assert!(ast.modules().is_empty());
     }
 
     #[test]
-    fn ast_adds_nodes_and_returns_ids() {
-        let mut ast = Ast::new(integer("1", Span::new(0, 1)));
-        let rhs = ast.push(integer("2", Span::new(4, 5)));
-        let lhs = ast.root_id();
-        let root = ast.push(Node::new(
-            NodeKind::Expr(Expr::new(ExprKind::Binary {
+    fn ast_adds_typed_nodes_and_returns_ids() {
+        let mut ast = Ast::new();
+        let lhs = ast.push_expr(integer("1", Span::new(0, 1)));
+        let rhs = ast.push_expr(integer("2", Span::new(4, 5)));
+        let root = ast.push_expr(Expr::new(
+            ExprKind::Binary {
                 lhs,
                 op: BinaryOp::Add,
                 rhs,
-            })),
+            },
             Span::new(0, 5),
         ));
 
+        assert_eq!(lhs.index(), 0);
         assert_eq!(rhs.index(), 1);
-        assert!(ast.set_root(root));
-        assert_eq!(ast.root().span, Span::new(0, 5));
+        assert_eq!(root.index(), 2);
+        assert_eq!(ast.expr(root).span, Span::new(0, 5));
     }
 
     #[test]
     fn ast_rejects_invalid_root_ids() {
-        let mut ast = Ast::new(integer("1", Span::new(0, 1)));
+        let mut ast = Ast::new();
 
-        assert!(!ast.set_root(NodeId(1)));
-        assert_eq!(ast.root_id(), NodeId(0));
-        assert!(ast.get_node(NodeId(1)).is_none());
+        assert!(!ast.set_root(ModuleId(0)));
+        assert_eq!(ast.root_id(), None);
+        assert!(ast.get_module(ModuleId(0)).is_none());
     }
 
     #[test]
     fn ast_represents_function_items_with_type_refs() {
-        let mut ast = Ast::new(Node::new(
-            NodeKind::Module(Module::new(Vec::new())),
-            Span::new(0, 0),
-        ));
+        let mut ast = Ast::new();
         let param_ty = TypeRef::named("Enemy", Span::new(16, 21));
         let return_ty = TypeRef::named("float", Span::new(26, 31));
-        let function = ast.push(Node::new(
-            NodeKind::Item(Item::new(ItemKind::Function(Function::new(
+        let body_expr = ast.push_expr(integer("1", Span::new(37, 38)));
+        let body_stmt = ast.push_stmt(Stmt::new(StmtKind::Expr(body_expr), Span::new(37, 38)));
+        let function = ast.push_item(Item::new(
+            ItemKind::Function(Function::new(
                 Ident::new("read_hp", Span::new(3, 10)),
                 vec![Param::new(
                     Ident::new("enemy", Span::new(11, 16)),
                     Some(param_ty.clone()),
                 )],
                 Some(return_ty.clone()),
-                Vec::new(),
-            )))),
-            Span::new(0, 32),
+                vec![body_stmt],
+            )),
+            Span::new(0, 38),
         ));
-        let module = ast.push(Node::new(
-            NodeKind::Module(Module::new(vec![function])),
-            Span::new(0, 32),
-        ));
+        let module = ast.push_module(Module::new(vec![function], Span::new(0, 38)));
 
         assert!(ast.set_root(module));
-        assert_eq!(ast.root_id(), module);
+        assert_eq!(ast.root_id(), Some(module));
         assert_eq!(
             param_ty.kind,
             TypeRefKind::Named(Ident::new("Enemy", Span::new(16, 21)))
@@ -350,10 +416,12 @@ mod tests {
             return_ty.kind,
             TypeRefKind::Named(Ident::new("float", Span::new(26, 31)))
         );
+        assert_eq!(ast.root().expect("root module").items, vec![function]);
 
-        let NodeKind::Module(module) = &ast.root().kind else {
-            panic!("root should be a module");
+        let ItemKind::Function(function) = &ast.item(function).kind else {
+            panic!("item should be a function");
         };
-        assert_eq!(module.items, vec![function]);
+        assert_eq!(function.body, vec![body_stmt]);
+        assert_eq!(ast.stmt(body_stmt).kind, StmtKind::Expr(body_expr));
     }
 }
